fab5d5fcde3c57150fee868e37711330d636137a
thanh05012003 <0395358670b@gmail.com>
2022-11-15 12:03:50 +0700

    Initial commit

---
+++ birthday-master/config.js
@@ -0,0 +1,37 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "Hey chào cậu",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "Hôm nay là 24/11/2022",
+        "là một ngày đặc biệt nhỉ",  // 同上...
+        "ngày mà cậu 19 tuổi",
+        "thời gian trôi quá nhanh, phải không",
+        "hy vọng mọi thứ sẽ tốt hơn",
+        "chúc Trâm Anh sinh nhật vui vẻ",
+        "cầu mong cho những giấc mơ của cậu đều thành sự thật",
+        "may mắn sẽ đến thăm cậu hàng ngày",
+        "chúc cậu ngày càng xinh hơn",
+        "cười nhiều hơn",
+        "thành công hơn",
+        "chúc cậu tuổi 19 thật là ý nghĩa",
+        "happy birthday",
+    ],
+    
+    imgs: {
+        "happy birthday": "./imgs/0587da143b94fdcaa485.jpg",
+    },
+   
+    desc: {
+        turn_on: "Bắt đầu",
+        play: "Âm nhạc",
+        bannar_coming: "Màu sắc",
+        balloons_flying: "Có vẻ như thiếu cái gì đó ?",
+        cake_fadein: "Bánh?",
+        light_candle: "Nến?",
+        wish_message: "happy birthday",
+        story: "A MESSAGE FOR YOU",
+        Phao_Hoa: "Pháo Hoa",
+    }
+};
